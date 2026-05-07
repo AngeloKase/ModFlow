3fb7d6bffd12f61d3839f139a50a8e099a263f2a
Vitor Hugo <vitor.hugooliver10@outlook.com>
2026-05-07 01:55:49 -0300

    Colocando a função de editar e status da tarefa
    adicionando a edição de tarefa e o status de concluida, em andamento e não iniciado no código

--- src/main.py
+++ src/main.py
@@ -7,7 +7,8 @@ tarefas = []
 def adicionar_tarefa():
     tarefa = entrada.get()
     if tarefa != "":
-        tarefas.append(tarefa)
+        prioridade = prioridade_var.get()
+        tarefas.append(f"[{prioridade}] {tarefa}")
         atualizar_lista()
         entrada.delete(0, tk.END)
 
@@ -26,6 +27,21 @@ def remover_tarefa():
         tarefas.pop(indice)
         atualizar_lista()
 
+# fun para editar a tarefa
+def editar_tarefa():
+    selecionada = lista.curselection()
+
+    if selecionada:
+        indice = selecionada[0]
+        
+        nova_tarefa = entrada.get()
+
+        if nova_tarefa != "":
+            prioridade = prioridade_var.get()
+            tarefas[indice] = f"[{prioridade}] {nova_tarefa}"
+            atualizar_lista()
+            entrada.delete(0, tk.END)
+
 
 janela = tk.Tk()
 janela.title("ModFlow - Gerenciador de Tarefas")
@@ -34,6 +50,18 @@ janela.title("ModFlow - Gerenciador de Tarefas")
 entrada = tk.Entry(janela, width=40)
 entrada.pack(pady=10)
 
+# prioridade da tarefa
+prioridade_var = tk.StringVar()
+prioridade_var.set("Média")
+
+menu_prioridade = tk.OptionMenu(
+    janela,
+    prioridade_var,
+    "Alta",
+    "Média",
+    "Baixa"
+)
+
 # botao do adicionar
 botao = tk.Button(janela, text="Adicionar Tarefa", command=adicionar_tarefa)
 botao.pack()
@@ -41,6 +69,10 @@ botao.pack()
 # botao do remover
 botao_remover = tk.Button(janela, text="Remover Tarefa", command=remover_tarefa)
 botao_remover.pack()
+
+# botao para editar
+botao_editar = tk.Button(janela, text="Editar Tarefa", command=editar_tarefa)
+botao_editar.pack()
 
 # lista de tarefas
 lista = tk.Listbox(janela, width=50)
