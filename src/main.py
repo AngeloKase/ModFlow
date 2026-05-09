bd7f9de6801ec1781b1be746f16ba60719de8314
Vitor Hugo <vitor.hugooliver10@outlook.com>
2026-05-09 05:34:43 -0300

    melhorando main com alguns detalhes
    mudanças visuais e adição do modo claro ou escuro ao aplicativo

--- src/main.py
+++ src/main.py
@@ -2,39 +2,197 @@ import customtkinter as ctk
 from PIL import Image
 from ui.menu import abrir_menu
 
-# tema da janela
+# =========================
+# CONFIGURAÇÕES INICIAIS
+# =========================
+
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
 
-# Janela principal
+
+
+
+# =========================
+# CORES
+# =========================
+
+BG_COLOR = ("#f5f5f5", "#111111")
+CARD_COLOR = ("#ffffff", "#181818")
+TEXT_COLOR = ("#111111", "#ffffff")
+SECOND_TEXT = ("#555555", "#aaaaaa")
+
+PURPLE = "#c77dff"
+PURPLE_HOVER = "#a855f7"
+
+# =========================
+# JANELA PRINCIPAL
+# =========================
+
 app = ctk.CTk()
 
 app.title("ModFlow")
 app.geometry("1200x800")
-app.resizable(True, True)
+
+# fullscreen
+app.attributes("-fullscreen", True)
+
+app.configure(
+    fg_color=BG_COLOR
+)
 
 
-# cores utilizadas
+# =========================================
+# FUNÇÕES FULLSCREEN
+# =========================================
 
-BG_COLOR = "#121212"
-PURPLE = "#c77dff"
+fullscreen = True
 
-app.configure(fg_color=BG_COLOR)
 
-# frame principal 
+def sair_fullscreen(event=None):
+
+    global fullscreen
+
+    fullscreen = False
+
+    app.attributes(
+        "-fullscreen",
+        False
+    )
+
+
+def entrar_fullscreen(event=None):
+
+    global fullscreen
+
+    fullscreen = True
+
+    app.attributes(
+        "-fullscreen",
+        True
+    )
+
+
+# =========================================
+# BINDS
+# =========================================
+
+# ESC sai da tela cheia
+app.bind(
+    "<Escape>",
+    sair_fullscreen
+)
+
+# F11 entra em tela cheia
+app.bind(
+    "<F11>",
+    entrar_fullscreen
+)
+
+
+
+# =========================
+# FUNÇÕES
+# =========================
+
+modo_atual = "dark"
+
+
+def trocar_tema():
+
+    global modo_atual
+
+    if modo_atual == "dark":
+
+        ctk.set_appearance_mode("light")
+
+        tema_btn.configure(
+            text="☀"
+        )
+
+        modo_atual = "light"
+
+    else:
+
+        ctk.set_appearance_mode("dark")
+
+        tema_btn.configure(
+            text="🌙"
+        )
+
+        modo_atual = "dark"
+
+
+def sair_fullscreen(event):
+
+    app.attributes(
+        "-fullscreen",
+        False
+    )
+
+
+# ESC para sair do fullscreen
+app.bind(
+    "<Escape>",
+    sair_fullscreen
+)
+
+# =========================
+# FRAME CENTRAL
+# =========================
 
 frame = ctk.CTkFrame(
     app,
-    fg_color="transparent"
+    width=700,
+    height=700,
+    corner_radius=25,
+    fg_color=CARD_COLOR,
+    border_width=2,
+    border_color=PURPLE
 )
 
-frame.pack(expand=True)
+frame.place(
+    relx=0.5,
+    rely=0.5,
+    anchor="center"
+)
 
-# icone do nosso servidor
+# impedir resize automático
+frame.pack_propagate(False)
+
+# =========================
+# BOTÃO TEMA
+# =========================
+
+tema_btn = ctk.CTkButton(
+    frame,
+    text="🌙",
+    width=45,
+    height=45,
+    corner_radius=100,
+    fg_color=PURPLE,
+    hover_color=PURPLE_HOVER,
+    font=("Segoe UI", 18),
+    command=trocar_tema
+)
+
+tema_btn.place(
+    relx=0.92,
+    rely=0.05,
+    anchor="center"
+)
+
+# =========================
+# LOGO
+# =========================
+
 imagem = ctk.CTkImage(
-    light_image=Image.open("assets/server_icon.png"),
-    dark_image=Image.open("assets/server_icon.png"),
-    size=(150, 150)
+    light_image=Image.open(
+        "assets/server_icon.png"
+    ),
+    dark_image=Image.open(
+        "assets/server_icon.png"
+    ),
+    size=(480, 300)
 )
 
 logo = ctk.CTkLabel(
@@ -43,43 +201,75 @@ logo = ctk.CTkLabel(
     text=""
 )
 
-logo.pack(pady=(20, 10))
+logo.pack(
+    pady=(40, 20)
+)
 
-# titulo do aplicativo
+# =========================
+# TÍTULO
+# =========================
 
 titulo = ctk.CTkLabel(
     frame,
     text="ModFlow",
-    font=("Segoe UI", 40, "bold"),
-    text_color="white"
+    font=("Segoe UI", 42, "bold"),
+    text_color=TEXT_COLOR
 )
 
-titulo.pack(pady=(10, 5))
+titulo.pack()
 
-# descrevendo o app
+# =========================
+# DESCRIÇÃO
+# =========================
+
 descricao = ctk.CTkLabel(
     frame,
     text="Painel de gerenciamento de tarefas para moderadores do Discord",
     font=("Segoe UI", 16),
-    text_color="#aaaaaa"
+    text_color=SECOND_TEXT
 )
 
-descricao.pack(pady=(0, 30))
+descricao.pack(
+    pady=(10, 40)
+)
 
-# botao para comecar
+# =========================
+# BOTÃO COMEÇAR
+# =========================
+
 botao_comecar = ctk.CTkButton(
     frame,
     text="Começar",
-    width=220,
-    height=50,
-    corner_radius=15,
+    width=260,
+    height=60,
+    corner_radius=20,
     fg_color=PURPLE,
-    hover_color="#acd855f7",
+    hover_color=PURPLE_HOVER,
     font=("Segoe UI", 18, "bold"),
-    command=lambda: abrir_menu(app)
+    command=lambda:
+        abrir_menu(app)
 )
 
 botao_comecar.pack()
 
-# Rodar app
+# =========================
+# FOOTER
+# =========================
+
+footer = ctk.CTkLabel(
+    frame,
+    text="ModFlow © 2026",
+    font=("Segoe UI", 12),
+    text_color=SECOND_TEXT
+)
+
+footer.pack(
+    side="bottom",
+    pady=20
+)
+
+# =========================
+# LOOP
+# =========================
+
 app.mainloop()
